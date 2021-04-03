350f31c735539105f57369f19c4c2fb61ab1e7a2
Guillaume Clemens <gcl@odoo.com>
2021-04-03 04:12:33 +0200

    IMP minor changes
    * minor formatting changes
    * header defined methods of Settings are now inline

--- include/App.hpp
+++ include/App.hpp
@@ -10,33 +10,34 @@
 class Renderer;
 
 namespace FSE {
-    class App
+    
+class App {
+public:
+    struct state_info {
+        bool normalized {true}; //used by sound synth, must be true for mandelbrot
+        bool sustain {true}; //should probly move to sound synth class
+        bool hide_orbit {false};
+        bool left_pressed {false};
+        bool dragging  {false};
+        bool should_take_screenshot {false};
+        bool should_show_help {false};
+        sf::Vector2<int> prevDrag;
+    } state;
+private:
+    Settings settings;
+    Fractal fractal;
+    Renderer renderer;
+public:
+    App (Settings app_settings) :
+    settings {app_settings},
+    renderer {app_settings}
     {
-        public:
-            struct state_info {
-                bool normalized {true}; //used by sound synth, must be true for mandelbrot
-                bool sustain {true}; //should probly move to sound synth class
-                bool hide_orbit {false};
-                bool left_pressed {false};
-                bool dragging  {false};
-                bool should_take_screenshot {false};
-                bool should_show_help {false};
-                sf::Vector2<int> prevDrag;
-            } state;
-        private:
-            Settings settings;
-            Fractal fractal;
-            Renderer renderer;
-        public:
-            App (Settings app_settings) :
-            settings {app_settings},
-            renderer {app_settings}
-            {
-                SetFractal(app_settings.starting_fractal);
-            }
-            int Start();
-            void SetFractal(const int& type);
-            void PollEvents();
-    };
+        SetFractal(app_settings.starting_fractal);
+    }
+    int Start();
+    void SetFractal(const int& type);
+    void PollEvents();
+};
+
 }
 #endif
--- include/Renderer.hpp
+++ include/Renderer.hpp
@@ -7,69 +7,68 @@
 #include "Settings.hpp"
 
 namespace FSE {
-        class HelpMenu 
-    {
-        public:
-            sf::RectangleShape background;
-            sf::Font font;
-            sf::Text help_text;
-        public:
-            void Reset (const sf::Vector2<float>& window_res, const std::string& path);
-            void SetSize(const sf::Vector2<float>& window_res);
-    };
+    
+class HelpMenu {
+public:
+    sf::RectangleShape background;
+    sf::Font font;
+    sf::Text help_text;
+public:
+    void Reset (const sf::Vector2<float>& window_res, const std::string& path);
+    void SetSize(const sf::Vector2<float>& window_res);
+};
 
-    class Renderer
-    {
-        public:
-            bool is_fullscreen {false};
-            bool help_enabled {false};
-            bool julia_drag {false};
-            bool use_color {false};
-            int frame_counter {0};
-            int max_iters {1000};
-            float cam_zoom {200.0F};
-            float cam_zoom_dest {200.0F};
-            sf::Vector2<float> window_res;
-            sf::Vector2<float> window_res_tr;
-            sf::Vector2<float> cam_world;
-            sf::Vector2<int> cam_dest_screen;
-            sf::Vector2<float> cam_dest_world;
-            sf::Vector2<float> julia_offset {1e8F, 1e8F};
-            struct state_info {
+class Renderer {
+public:
+    bool is_fullscreen {false};
+    bool help_enabled {false};
+    bool julia_drag {false};
+    bool use_color {false};
+    int frame_counter {0};
+    int max_iters {1000};
+    float cam_zoom {200.0F};
+    float cam_zoom_dest {200.0F};
+    sf::Vector2<float> window_res;
+    sf::Vector2<float> window_res_tr;
+    sf::Vector2<float> cam_world;
+    sf::Vector2<int> cam_dest_screen;
+    sf::Vector2<float> cam_dest_world;
+    sf::Vector2<float> julia_offset {1e8F, 1e8F};
+    struct state_info {
 
-            } state;
+    } state;
+private:
+    static const sf::BlendMode BlendAlpha;
+    static const sf::BlendMode BlendIgnoreAlpha;
+    sf::ContextSettings context_settings;
+    sf::RenderWindow window;
+    sf::RenderTexture render_target;
+    sf::Shader shader;
+    sf::RectangleShape viewport;
+    HelpMenu help;
+public:
+    Renderer (const Settings& app_settings);
 
-        private:
-            static const sf::BlendMode BlendAlpha;
-            static const sf::BlendMode BlendIgnoreAlpha;
-            sf::ContextSettings context_settings;
-            sf::RenderWindow window;
-            sf::RenderTexture render_target;
-            sf::Shader shader;
-            sf::RectangleShape viewport;
-            HelpMenu help;
-        public:
-            Renderer (const Settings& app_settings);
+    void Fractal_Render();
+    void MakeWindow(Settings);
+    // camera manipulation
+    void ApplyZoom();
+    void ResetCam();
+    // window manipulation
+    void SetWindowRes(const int&, const int&);
+    void ResizeWindow(const int&, const int&);
+    void CloseWindow();
+    bool IsWindowOpen();
+    bool PollWindowForEvents(sf::Event&);
+    sf::Vector2<int> GetMousePosition();
+    // coordinate conversion
+    void ScreenToWorld(sf::Vector2<int>, sf::Vector2<float>&);
+    void WorldToScreen(sf::Vector2<float>, sf::Vector2<int>&);
+    // misc
+    void GrabJuliaOffset();
+    sf::Shader& GetShader();
+    void TakeScreenshot();
+};
 
-            void Fractal_Render();
-            void MakeWindow(Settings);
-            // camera manipulation
-            void ApplyZoom();
-            void ResetCam();
-            // window manipulation
-            void SetWindowRes(const int&, const int&);
-            void ResizeWindow(const int&, const int&);
-            void CloseWindow();
-            bool IsWindowOpen();
-            bool PollWindowForEvents(sf::Event&);
-            sf::Vector2<int> GetMousePosition();
-            // coordinate conversion
-            void ScreenToWorld(sf::Vector2<int>, sf::Vector2<float>&);
-            void WorldToScreen(sf::Vector2<float>, sf::Vector2<int>&);
-            // misc
-            void GrabJuliaOffset();
-            sf::Shader& GetShader();
-            void TakeScreenshot();
-    };
 }
 #endif
