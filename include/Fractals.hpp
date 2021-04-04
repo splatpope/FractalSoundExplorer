643ce0361966763a08cb94e6c0101924cc2b3a90
Guillaume Clemens <gcl@odoo.com>
2021-04-04 18:52:01 +0200

    IMP made software fractals use gamma complex type

--- include/Fractals.hpp
+++ include/Fractals.hpp
@@ -1,14 +1,14 @@
 #ifndef FRACTALS_INCLUDED
 #define FRACTALS_INCLUDED
-#include <complex>
-void mandelbrot(double& x, double& y, double cx, double cy);
-void burning_ship(double& x, double& y, double cx, double cy);
-void feather(double& x, double& y, double cx, double cy);
-void sfx(double& x, double& y, double cx, double cy);
-void henon(double& x, double& y, double cx, double cy);
-void duffing(double& x, double& y, double cx, double cy);
-void ikeda(double& x, double& y, double cx, double cy);
-void chirikov(double& x, double& y, double cx, double cy);
+#include "Gamma/Types.h"
+void mandelbrot(gam::Complex<double>& p, gam::Complex<double> c);
+void burning_ship(gam::Complex<double>& p, gam::Complex<double> c);
+void feather(gam::Complex<double>& p, gam::Complex<double> c);
+void sfx(gam::Complex<double>& p, gam::Complex<double> c);
+void henon(gam::Complex<double>& p, gam::Complex<double> c);
+void duffing(gam::Complex<double>& p, gam::Complex<double> c);
+void ikeda(gam::Complex<double>& p, gam::Complex<double> c);
+void chirikov(gam::Complex<double>& p, gam::Complex<double> c);
 //Fractal abstraction definition
-typedef void (*Fractal)(double&, double&, double, double);
+typedef void (*Fractal)(gam::Complex<double>&, gam::Complex<double>);
 #endif
--- src/Fractals.cpp
+++ src/Fractals.cpp
@@ -1,55 +1,44 @@
+#include <cmath>
 #include "Fractals.hpp"
 
-void mandelbrot(double& x, double& y, double cx, double cy) {
-  double nx {x*x - y*y + cx};
-  double ny {2.0*x*y + cy};
-  x = nx;
-  y = ny;
+void mandelbrot(gam::Complex<double>& p, gam::Complex<double> c) {
+  p = p*p + c;
 }
-void burning_ship(double& x, double& y, double cx, double cy) {
-  double nx {x*x - y*y + cx};
-  double ny {2.0*std::abs(x*y) + cy};
-  x = nx;
-  y = ny;
+void burning_ship(gam::Complex<double>& p, gam::Complex<double> c) {
+  gam::Complex<double> p_abs{gam::abs(p)};
+  p = p*p + c;
 }
-void feather(double& x, double& y, double cx, double cy) {
-  std::complex<double> z{x, y};
-  std::complex<double> z2{x*x, y*y};
-  std::complex<double> c{cx, cy};
-  std::complex<double> one{1.0, 0.0};
-  z = z*z*z/(one + z2) + c;
-  x = z.real();
-  y = z.imag();
+void feather(gam::Complex<double>& p, gam::Complex<double> c) {
+  gam::Complex<double> p2{pow(p.r, 2), pow(p.i, 2)};
+  gam::Complex<double> one{1.0, 0.0};
+  p = p*p*p/(one + p2) + c;
 }
-void sfx(double& x, double& y, double cx, double cy) {
-  std::complex<double> z{x, y};
-  std::complex<double> c2{cx*cx, cy*cy};
-  z = z * (x*x + y*y) - (z * c2);
-  x = z.real();
-  y = z.imag();
+void sfx(gam::Complex<double>& p, gam::Complex<double> c) {
+  gam::Complex<double> c2{pow(c.r, 2), pow(c.i, 2)};
+  p = p * p.normSqr() - (p * c2);
 }
-void henon(double& x, double& y, double cx, double cy) {
-  double nx {1.0 - cx*x*x + y};
-  double ny = {cy*x};
-  x = nx;
-  y = ny;
+void henon(gam::Complex<double>& p, gam::Complex<double> c) {
+  double nx {1.0 - c.r * p.r * p.r + p.i};
+  double ny {c.i * p.r};
+  p.r = nx;
+  p.i = ny;
 }
-void duffing(double& x, double& y, double cx, double cy) {
-  double nx {y};
-  double ny {-cy*x + cx*y - y*y*y};
-  x = nx;
-  y = ny;
+void duffing(gam::Complex<double>& p, gam::Complex<double> c) {
+  double nx {p.i};
+  double ny {-c.i * p.r + c.r * p.i - pow(p.i, 3)};
+  p.r = nx;
+  p.i = ny;
 }
-void ikeda(double& x, double& y, double cx, double cy) {
-  double t {0.4 - 6.0 / (1.0 + x*x + y*y)};
+void ikeda(gam::Complex<double>& p, gam::Complex<double> c) { // apparently this is only one in a family of parametrized functions
+  double t {0.4 - 6.0 / (1.0 + pow(p.r, 2) + pow(p.i, 2))};
   double st {std::sin(t)};
   double ct {std::cos(t)};
-  double nx {1.0 + cx*(x*ct - y*st)};
-  double ny {cy*(x*st + y*ct)};
-  x = nx;
-  y = ny;
+  double nx {1.0 + c.r * (p.r * ct - p.i * st)};
+  double ny {c.i * (p.r * st + p.i * ct)};
+  p.r = nx;
+  p.i = ny;
 }
-void chirikov(double& x, double& y, double cx, double cy) {
-  y += cy*std::sin(x);
-  x += cx*y;
+void chirikov(gam::Complex<double>& p, gam::Complex<double> c) {
+  p.i += c.i *std::sin(p.r);
+  p.r += c.r * p.i;
 }
