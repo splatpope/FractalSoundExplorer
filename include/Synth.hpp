82aac80444fc7c1e3fd192bc0d07a1e33950e9d7
Guillaume Clemens <gcl@odoo.com>
2021-05-08 16:40:26 +0200

    IMP no more clipping
    Changed envelope settings to sensible values (ie less than the actual
    note changing period) so it doesnt clip anymore

--- include/Synth.hpp
+++ include/Synth.hpp
@@ -18,7 +18,7 @@ public:
         initAudio(sampleRate);
     }
 
-    inline void initAudio(double framesPerSecond, unsigned framesPerBuffer=128, unsigned outChans=2, unsigned inChans=0) {
+    inline void initAudio(double framesPerSecond, unsigned framesPerBuffer=4096, unsigned outChans=2, unsigned inChans=0) {
         audio_io.framesPerSecond(framesPerSecond);
         audio_io.framesPerBuffer(framesPerBuffer);
         audio_io.channelsOut(outChans);
@@ -52,14 +52,16 @@ public:
     Complex<> orbit {0.0f, 0.0f};
     Fractal fractal;
     unsigned int max_freq;
+    float vol;
 
     inline FractalSynth(double sampleRate, unsigned int max_freq) : 
     timer {5},
     SynthBase {sampleRate},
-    max_freq {max_freq}
+    max_freq {max_freq},
+    vol {1.0f}
     {
-        env.attack(0.02);
-        env.decay(0.5);
+        env.attack(0.1);
+        env.decay(0.09);
         timer.phaseMax();
         osc.freq(max_freq);
     }
@@ -67,25 +69,32 @@ public:
     inline void onAudio (gam::AudioIOData& io) {
         while (io()) {
             if (timer()) {
-                nextFreq(fractal);
+                nextPoint(fractal);
                 env.reset();
             }
 
-            io.out(0) = io.out(1) = osc() * env() * 0.2f;
+            io.out(0) = io.out(1) = osc() * 0.2f * vol * env();
         }
     }
 
     inline void setFreqFromCpx (Complex<> cp) {
         gam::real freq = max_freq * std::abs((std::arg(cp) / M_PI));
         // turns out clamping the freqs is good
-        freq = std::max(std::min(static_cast<float>(max_freq), freq), 22.0f);
+        freq = std::max(std::min(static_cast<float>(max_freq), freq), 44.0f);
         osc.freq(freq);
     }
 
-    inline void nextFreq(Fractal fractal) {
+    inline void setVolFromCpx (Complex<> cp) {
+        vol = 1.0f / std::abs(cp) - 0.5f;
+        vol = std::min(vol, 1.0f);
+        //printf("\n%f\n", vol);
+    }
+
+    inline void nextPoint(Fractal fractal) {
         //Complex<> orbit = orbit_start;
         fractal(orbit, orbit_start);
         setFreqFromCpx(orbit);
+        setVolFromCpx(orbit);
         //orbit_start = orbit;
     }
 
@@ -93,7 +102,7 @@ public:
         if (!isRunning) {
             SynthBase::start();
         } else {
-            nextFreq(fractal);
+            nextPoint(fractal);
         }
     }
 };
